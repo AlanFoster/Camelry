06eadcb85d6e9222de0faaee4197d41066b5a34f
Alan Foster <alan@alanfoster.me>
2013-03-18 22:43:18 +0000

    Add language injection for simple into blueprint/spring xml dsl :) I wonder how I can do this automatically with the Java DSL? Update the plugin.xml to wireup the extensions

---
+++ src/me/alanfoster/intellij/camel/simple/language/tooling/languageinjector/SimpleLanguageInjector.java
@@ -0,0 +1,39 @@
+package me.alanfoster.intellij.camel.simple.language.tooling.languageinjector;
+
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.InjectedLanguagePlaces;
+import com.intellij.psi.LanguageInjector;
+import com.intellij.psi.PsiLanguageInjectionHost;
+import com.intellij.psi.xml.XmlTag;
+import com.intellij.psi.xml.XmlText;
+import me.alanfoster.intellij.camel.simple.language.SimpleLanguage;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * This injector deals with injecting the Simple language into the camel
+ * DSL roots
+ *
+ * @author Alan Foster
+ * @version 1.0.0-SNAPSHOT
+ */
+public class SimpleLanguageInjector implements LanguageInjector {
+
+    @Override
+    public void getLanguagesToInject(@NotNull PsiLanguageInjectionHost psiLanguageInjectionHost,
+                                     @NotNull InjectedLanguagePlaces injectedLanguagePlaces) {
+        // Attempt to match an xml element with a parent node which has a local name
+        // of simple, IE <simple>${simple-expression}</simple>
+        if (psiLanguageInjectionHost instanceof XmlText) {
+            XmlText xmlText = (XmlText) psiLanguageInjectionHost;
+            XmlTag xmlTagParent = xmlText.getParentTag();
+
+            String xmlTagParentLocalName = xmlTagParent.getLocalName();
+
+            if(xmlTagParentLocalName != null && xmlTagParentLocalName.equals("simple")){
+                TextRange xmlTextRange = psiLanguageInjectionHost.getTextRange();
+                TextRange expressionTextRange = TextRange.from(0, psiLanguageInjectionHost.getTextLength());
+                injectedLanguagePlaces.addPlace(SimpleLanguage.INSTANCE, expressionTextRange, null, null);
+            }
+        }
+    }
+}
