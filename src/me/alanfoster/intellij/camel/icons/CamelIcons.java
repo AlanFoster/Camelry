893517c46822049e8229f6c8307a5aefb3bfed22
Alan Foster <alan@alanfoster.me>
2013-03-18 14:42:19 +0000

    Add SimpleLanguage and File data so that intellij can pick up our 'simple' files. Added the Camel icon from FuseIDE - I'll have to find out if this is OK or not!

---
+++ src/me/alanfoster/intellij/camel/icons/CamelIcons.java
@@ -0,0 +1,18 @@
+package me.alanfoster.intellij.camel.icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+/**
+ * Stores all of the Icons used by this plugin
+ *
+ * public static access to such data seems to be the common way
+ * of doing this in IntelliJ plugins...
+ *
+ * @author Alan Foster
+ * @version 1.0.0-SNAPSHOT
+ */
+public class CamelIcons {
+    public static final Icon CAMEL = IconLoader.getIcon("/me/alanfoster/intellij/camel/icons/camel.png");
+}
