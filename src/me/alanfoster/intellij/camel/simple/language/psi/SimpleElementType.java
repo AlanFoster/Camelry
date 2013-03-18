aada24738baaff6ac75380f48adc4992bc409a9b
Alan Foster <alan@alanfoster.me>
2013-03-18 17:53:10 +0000

    Add basic grammar definition for the simple language

---
+++ src/me/alanfoster/intellij/camel/simple/language/psi/SimpleElementType.java
@@ -0,0 +1,18 @@
+package me.alanfoster.intellij.camel.simple.language.psi;
+
+import com.intellij.psi.tree.IElementType;
+import me.alanfoster.intellij.camel.simple.language.SimpleLanguage;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Represents an Element. IE, a top level element of a tree
+ *
+ * @author Alan Foster
+ * @version 1.0.0-SNAPSHOT
+ */
+public class SimpleElementType extends IElementType {
+    SimpleElementType(@NotNull @NonNls String debugName) {
+        super(debugName, SimpleLanguage.INSTANCE);
+    }
+}
