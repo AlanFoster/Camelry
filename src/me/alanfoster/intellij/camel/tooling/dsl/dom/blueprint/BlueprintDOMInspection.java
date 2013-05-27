1006a20a869b0396ffe43f8bf1f86f9ec572707d
Alan Foster <alan@alanfoster.me>
2013-05-27 21:34:20 +0100

    Add localInspection element to plugin.xml that points to BlueprintDOMInspection.java, so that basic dom errors are shown, ie making use of the annotations on the DomElement interfaces

---
+++ src/me/alanfoster/intellij/camel/tooling/dsl/dom/blueprint/BlueprintDOMInspection.java
@@ -0,0 +1,13 @@
+package me.alanfoster.intellij.camel.tooling.dsl.dom.blueprint;
+
+import com.intellij.util.xml.highlighting.BasicDomElementsInspection;
+
+/**
+ * @author Alan Foster
+ * @version 1.0.0-SNAPSHOT
+ */
+public class BlueprintDOMInspection extends BasicDomElementsInspection {
+    public BlueprintDOMInspection() {
+        super(Blueprint.class);
+    }
+}
