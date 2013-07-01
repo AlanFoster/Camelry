6fa048fa0d88aee3a41684abb8bc8e84944b4649
Alan Foster <alan@alanfoster.me>
2013-07-01 20:49:02 +0100

    Further i18n support for #26

--- src/me/alanfoster/camelus/camel/tooling/CreateCamelFile.java
+++ src/me/alanfoster/camelus/camel/tooling/CreateCamelFile.java
@@ -1,4 +1,4 @@
-package me.alanfoster.camelus.camel.tooling.dsl.dom;
+package me.alanfoster.camelus.camel.tooling;
 
 import com.intellij.ide.actions.CreateFileFromTemplateAction;
 import com.intellij.ide.actions.CreateFileFromTemplateDialog;
@@ -8,13 +8,15 @@ import com.intellij.psi.PsiDirectory;
 import me.alanfoster.camelus.icons.PluginIcons;
 import me.alanfoster.camelus.blueprint.dom.BlueprintFileTemplateManager;
 
+import static me.alanfoster.camelus.CamelusBundle.message;
+
 /**
  * Adds the option to create a new camel related file in the
  * 'New' context menu of camelus.
- *
+ * <p/>
  * Note this has been registered via an action in the META-INF/plugin.xml
  * file.
- *
+ * <p/>
  * This also makes use of the registered BlueprintFileTemplateManager file templates
  * so make sure to check that out for further details.
  *
@@ -25,28 +27,28 @@ import me.alanfoster.camelus.blueprint.dom.BlueprintFileTemplateManager;
 public class CreateCamelFile extends CreateFileFromTemplateAction {
 
     public CreateCamelFile() {
-        super("Create a camel file",
-              "Create a new camel file",
-               PluginIcons.CAMEL);
+        super(message("camelus.templates.text"),
+                message("camelus.templates.description"),
+                PluginIcons.CAMEL);
     }
 
     @Override
     protected void buildDialog(Project project, PsiDirectory directory, CreateFileFromTemplateDialog.Builder builder) {
-        builder.setTitle("Create new Camel File")
-                .addKind("Blueprint Definition", PluginIcons.CAMEL, BlueprintFileTemplateManager.BLUEPRINT_DEFINITION_FILE)
-                .addKind("Blueprint Test File", PluginIcons.CAMEL, BlueprintFileTemplateManager.BLUEPRINT_TEST_FILE);
+        builder.setTitle(message("camelus.templates.title"))
+                .addKind(message("camelus.templates.blueprint.route"), PluginIcons.CAMEL, BlueprintFileTemplateManager.BLUEPRINT_DEFINITION_FILE)
+                .addKind(message("camelus.templates.blueprint.test"), PluginIcons.CAMEL, BlueprintFileTemplateManager.BLUEPRINT_TEST_FILE);
     }
 
     @Override
     protected String getActionName(PsiDirectory directory, String newName, String templateName) {
-        return "This is the action name";
+        return message("camelus.templates.actionname");
     }
 
     /**
      * {@inheritDoc}.
      * Decide when to show the CreateCamelFile option to the
      * user.
-     *
+     * <p/>
      * For the minute, always.
      *
      * @param e
