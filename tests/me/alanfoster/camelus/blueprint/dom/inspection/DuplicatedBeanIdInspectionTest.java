bbc81c4bdcee08ca479be1cb679d3f4a8a0db6a8
Alan Foster <alan@alanfoster.me>
2013-07-08 18:48:25 +0100

    #39 - Add test for duplicated bean inspection within the same module, by explicitly registered an inspection with the testing fixture

--- tests/me/alanfoster/camelus/blueprint/dom/inspection/DuplicatedBeanIdInspectionTest.java
+++ tests/me/alanfoster/camelus/blueprint/dom/inspection/DuplicatedBeanIdInspectionTest.java
@@ -2,6 +2,7 @@ package me.alanfoster.camelus.blueprint.dom.inspection;
 
 import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase;
 import me.alanfoster.camelus.TestHelper;
+import me.alanfoster.camelus.blueprint.inspectors.DuplicatedBeanIdInspection;
 
 import static me.alanfoster.camelus.blueprint.CamelusProjectDescriptorBuilder.CreateCamelusProject;
 
@@ -10,6 +11,14 @@ import static me.alanfoster.camelus.blueprint.CamelusProjectDescriptorBuilder.Cr
  * the same bundle
  */
 public class DuplicatedBeanIdInspectionTest extends LightCodeInsightFixtureTestCase {
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        // Explicitly register our inspection, otherwise intellij doesn't pick up the inspection!
+        myFixture.enableInspections(new DuplicatedBeanIdInspection());
+    }
+
     @Override
     public String getTestDataPath() {
         return TestHelper.getTestDataPath() + "/blueprint/dom/inspection";
@@ -20,14 +29,14 @@ public class DuplicatedBeanIdInspectionTest extends LightCodeInsightFixtureTestC
      * the camel method XML DSL.
      */
     // TODO Depends on more complex test structure setup
-    /*public void testDuplicatedBeanIdWithinSameBundle() {
+    public void testDuplicatedBeanIdWithinSameBundle() {
         CreateCamelusProject(myFixture)
-                .withBlueprintFiles("TwoThreeFourFiveBeans.xml", "TwoThreeFourFiveBeans.xml")
+                .withBlueprintFiles("TwoThreeFourFiveBeans.xml", "OneTwoThreeFourBeans.xml")
                 .withOpenedFile("OSGI-INF/blueprint/TwoThreeFourFiveBeans.xml");
 
         myFixture.checkHighlighting(true, true, true);
     }
-*/
+
     // TODO Write test for highlighting within test directories
 
 }
